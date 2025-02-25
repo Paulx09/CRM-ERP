82ae70ce615afc1d08b487a041beea5641459d33
PaulxA09 <pauloarmey18@gmail.com>
2025-02-25 16:50:47 -0500

    Actualizar la validación de contraseña y ajustar el uso de autenticación API en AuthController

--- api-crm-erp/app/Http/Controllers/AuthController.php
+++ api-crm-erp/app/Http/Controllers/AuthController.php
@@ -30,7 +30,8 @@ class AuthController extends Controller
         $validator = Validator::make(request()->all(), [
             'name' => 'required',
             'email' => 'required|email|unique:users',
-            'password' => 'required|confirmed|min:8',
+            // 'password' => 'required|confirmed|min:8',
+            'password' => 'required|min:8',
         ]);
  
         if($validator->fails()){
@@ -56,7 +57,7 @@ class AuthController extends Controller
     {
         $credentials = request(['email', 'password']);
  
-        if (! $token = auth()->attempt($credentials)) {
+        if (! $token = auth('api')->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
  
@@ -70,7 +71,7 @@ class AuthController extends Controller
      */
     public function me()
     {
-        return response()->json(auth()->user());
+        return response()->json(auth('api')->user());
     }
  
     /**
@@ -80,7 +81,7 @@ class AuthController extends Controller
      */
     public function logout()
     {
-        auth()->logout();
+        auth('api')->logout();
  
         return response()->json(['message' => 'Successfully logged out']);
     }
@@ -92,7 +93,7 @@ class AuthController extends Controller
      */
     public function refresh()
     {
-        return $this->respondWithToken(auth()->refresh());
+        return $this->respondWithToken(auth('api')->refresh());
     }
  
     /**
@@ -107,7 +108,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth('api')->factory()->getTTL() * 60
         ]);
     }
 }
